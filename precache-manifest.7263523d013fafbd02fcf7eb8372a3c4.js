dc7ebb14c4706e0ba6d458eb4f6678d8af6e9d20
VladOSSystem <linuxmint360@gmail.com>
2020-07-09 14:17:26 +0200

    Updates

---
+++ precache-manifest.7263523d013fafbd02fcf7eb8372a3c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca2899f83704002d8db2447bc352493",
+    "url": "/TestTask/index.html"
+  },
+  {
+    "revision": "94440184d570224cead4",
+    "url": "/TestTask/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0b01d12b2bdc53cc3331",
+    "url": "/TestTask/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94440184d570224cead4",
+    "url": "/TestTask/static/js/2.2ef0d592.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/TestTask/static/js/2.2ef0d592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b01d12b2bdc53cc3331",
+    "url": "/TestTask/static/js/main.eb71331e.chunk.js"
+  },
+  {
+    "revision": "60207095af5f0b94791e",
+    "url": "/TestTask/static/js/runtime-main.de4caf40.js"
+  }
+]);
