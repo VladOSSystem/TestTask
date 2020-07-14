f80bbc542657eda5d12cd04c4bbf39555a6b2b4c
VladOSSystem <linuxmint360@gmail.com>
2020-07-14 19:09:36 +0200

    Updates

--- precache-manifest.3e200fb2432c9f22f9263fe04fdf0eea.js
+++ precache-manifest.3e200fb2432c9f22f9263fe04fdf0eea.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac21d5dcffc103a03098b02aeee41843",
+    "revision": "336e16d26ea0882f95a1613f807a51f6",
     "url": "/TestTask/index.html"
   },
   {
-    "revision": "94440184d570224cead4",
+    "revision": "2d8fb5ac7cfd2cec4b3a",
     "url": "/TestTask/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "33ffa18fb20c152ff417",
+    "revision": "8676af6c484642b063ee",
     "url": "/TestTask/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "94440184d570224cead4",
-    "url": "/TestTask/static/js/2.2ef0d592.chunk.js"
+    "revision": "2d8fb5ac7cfd2cec4b3a",
+    "url": "/TestTask/static/js/2.9fd6d25d.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/TestTask/static/js/2.2ef0d592.chunk.js.LICENSE.txt"
+    "url": "/TestTask/static/js/2.9fd6d25d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "33ffa18fb20c152ff417",
-    "url": "/TestTask/static/js/main.4861da84.chunk.js"
+    "revision": "8676af6c484642b063ee",
+    "url": "/TestTask/static/js/main.6815957a.chunk.js"
   },
   {
     "revision": "60207095af5f0b94791e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TestTask/precache-manifest.072dbecd33fb72fe156e442606b03755.js"
+  "/TestTask/precache-manifest.3e200fb2432c9f22f9263fe04fdf0eea.js"
 );
 
 self.addEventListener('message', (event) => {
